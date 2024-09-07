f34dfd9fc0b3b2d516bb4749898d99ced5b3ea9b
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2024-09-07 12:48:26 +0900

    feat: improves traffic light component and implements styles

--- src/components/molecules/trafficLight/index.tsx
+++ src/components/molecules/trafficLight/index.tsx
@@ -1,19 +1,38 @@
+import { useState } from "react";
+import "./styles.css";
+
+type TrafficLightStateType = "stopped" | "calculating" | "complete";
+
 export function TrafficLight() {
+  const [state, setState] = useState<TrafficLightStateType>("stopped");
+
+  const handleClick = (newState: TrafficLightStateType) => {
+    setState(newState);
+  };
+
   return (
     <>
       <div className="container">
-        <div className="fire">
-          <label className="label red">
-            <input type="radio" name="radio" className="input" />
-          </label>
-          <label className="label orange">
-            <input type="radio" name="radio" className="input" />
-          </label>
-          <label className="label green">
-            <input type="radio" name="radio" className="input" />
+        <div className="trafficLight">
+          <div
+            className={`label red ${state === "stopped" ? "activeRed" : ""}`}
+            onClick={() => handleClick("stopped")}
+          ></div>
+          <div
+            className={`label yellow ${
+              state === "calculating" ? "activeYellow" : ""
+            }`}
+            onClick={() => handleClick("calculating")}
+          ></div>
+
+          <div
+            className={`label green ${
+              state === "complete" ? "activeGreen" : ""
+            }`}
+            onClick={() => handleClick("complete")}
+          >
             <span className="light-number">3</span>
-          </label>
-          <span className="selection"></span>
+          </div>
         </div>
       </div>
     </>
